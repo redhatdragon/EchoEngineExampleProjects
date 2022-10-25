80e17c5ec4c97193d65adfd9feba5f3c2cd26040
redhatdragon <paulabrams99@yahoo.com>
2022-10-25 04:18:29 -0400

    Added blank project example

---
+++ blank/main.cpp
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <IO_API/IO_API.h>
+#include <iostream>
+
+
+
+void appStart() {
+	// Ran at the start of our app...
+}
+
+void appLoop() {
+	// Ran every render frame of our app...
+}
+
+void appEnd() {
+	// Ran before quitting our app...
+}
---
+++ blank/setup.bat
@@ -0,0 +1,7 @@
+mkdir deps
+cd deps
+git clone https://github.com/redhatdragon/EchoEngine EchoEngine
+cd EchoEngine
+setup.bat
+cd ../
+cd ../
